faa69f9af4bf9cc0647cea8d48e9387d76df6fd0
jimSmyrnakis <102850351+jimSmyrnakis@users.noreply.github.com>
2026-02-16 22:15:11 +0200

    debug set up

--- Platform/Source/Debug/Debug.hpp
+++ Platform/Source/Debug/Debug.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
+#if defined(_MSC_VER)
+	#define breakpoint __debugbreak()
+#elif defined(__GNUC__)
+	#define breakpoint __builtin_trap()
+#else 
+	#error "Unsupported compiler"
+#endif 
+
+#ifdef _DEBUG 
+
+	#define ASSERT(expr) if (!expr) { breakpoint; }
+
+#elif defined(NDEBUG)
+	
+	#define ASSERT(expr) if (!expr) { exit(1); }
+
+#endif 
+
 
